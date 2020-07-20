080d4eb8a69006ef9d2e29947ecc82dfe901626f
Will Hart <hart.wl@gmail.com>
2020-07-20 16:14:11 +1000

    Setup shared package, move server to src

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,25 +1,25 @@
-import http from "http";
-import express from "express";
-import cors from "cors";
-import { Server } from "colyseus";
-import { monitor } from "@colyseus/monitor";
+import http from 'http'
+import express from 'express'
+import cors from 'cors'
+import { Server } from 'colyseus'
+import { monitor } from '@colyseus/monitor'
 // import socialRoutes from "@colyseus/social/express"
 
-import { MyRoom } from "./MyRoom";
+import { MyRoom } from './rooms/MyRoom'
 
-const port = Number(process.env.PORT || 2567);
+const port = Number(process.env.PORT || 2567)
 const app = express()
 
-app.use(cors());
+app.use(cors())
 app.use(express.json())
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 const gameServer = new Server({
   server,
-});
+})
 
 // register your room handlers
-gameServer.define('my_room', MyRoom);
+gameServer.define('my_room', MyRoom)
 
 /**
  * Register @colyseus/social routes
@@ -30,7 +30,7 @@ gameServer.define('my_room', MyRoom);
 // app.use("/", socialRoutes);
 
 // register colyseus monitor AFTER registering your room handlers
-app.use("/colyseus", monitor());
+app.use('/colyseus', monitor())
 
-gameServer.listen(port);
-console.log(`Listening on ws://localhost:${ port }`)
+gameServer.listen(port)
+console.log(`Listening on ws://localhost:${port}`)
