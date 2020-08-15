d7e52f6297929666219a402257ca204f8b112f81
William Hart <hart.wl@gmail.com>
2020-08-15 13:41:30 +1000

    Implement state machine / ECS test

--- packages/state-machine/src/StateMachineComponent.ts
+++ packages/state-machine/src/StateMachineComponent.ts
@@ -1,7 +1,13 @@
 import { Component } from '@colyseus/ecs'
 import { type } from '@colyseus/schema'
 
-export class StateMachineComponent<
+/**
+ * An abstract component that can be extended for individual state machines.
+ *
+ * A string enum should be provided as the generic type. See ecs.tests.ts for
+ * an example of usage.
+ */
+export abstract class StateMachineComponent<
   TStateEnum extends string
 > extends Component {
   @type('string') currentState?: TStateEnum
--- packages/state-machine/src/StateMachineSystem.ts
+++ packages/state-machine/src/StateMachineSystem.ts
@@ -1,20 +1,25 @@
 import { System } from '@colyseus/ecs'
 import { StateMachine, StateMachineTransitionMap } from './StateMachine'
 
+export interface StateMachineSystemAttributes<TStateEnum extends string> {
+  priority?: number
+  stateTransitions: StateMachineTransitionMap<TStateEnum>
+  initialState: TStateEnum
+  [key: string]: any
+}
+
 /**
- * An abstract state machine system that should be updated with transition logic
+ * An abstract state machine system that should be updated with transition logic.
+ *
+ * Ensure that the system is registered with the correct attributes type, see
+ * StateMachineSystemAttributes<TStateEnum>
  */
 export abstract class StateMachineSystem<
   TStateEnum extends string
 > extends System {
   protected stateMachine!: StateMachine<TStateEnum>
 
-  init(attributes: {
-    priority: number
-    stateTransitions: StateMachineTransitionMap<TStateEnum>
-    initialState: TStateEnum
-    [key: string]: any
-  }) {
+  init(attributes: StateMachineSystemAttributes<TStateEnum>) {
     this.stateMachine = new StateMachine(
       attributes.stateTransitions,
       attributes.initialState,
