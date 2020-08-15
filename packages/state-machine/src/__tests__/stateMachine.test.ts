7a15f4caf7a129f8b83345aba8049f5eb0a31922
William Hart <hart.wl@gmail.com>
2020-08-15 12:46:49 +1000

    Initial state machine implementation

---
+++ packages/state-machine/src/__tests__/stateMachine.test.ts
@@ -0,0 +1,96 @@
+import { StateMachine } from '../stateMachine'
+
+describe('State Machine', () => {
+  enum TestStates {
+    State1 = 'state1',
+    State2 = 'state2',
+    State3 = 'state3',
+  }
+
+  const getStateMachine = (
+    initialState: TestStates = TestStates.State2,
+  ): StateMachine<TestStates> => {
+    return new StateMachine<TestStates>(
+      {
+        [TestStates.State1]: [TestStates.State1, TestStates.State2],
+        [TestStates.State2]: [TestStates.State1],
+        [TestStates.State3]: [],
+      },
+      initialState,
+    )
+  }
+
+  describe('SMS - create state machines', () => {
+    it('SMS.01 - should create state machine without throwing', () => {
+      getStateMachine()
+    })
+
+    it('SMS.02 - should set the initial state', () => {
+      const sm1 = getStateMachine(TestStates.State1)
+      expect(sm1.currentState).toEqual(TestStates.State1)
+      const sm2 = getStateMachine(TestStates.State2)
+      expect(sm2.currentState).toEqual(TestStates.State2)
+    })
+  })
+
+  describe('SMT - available transitions', () => {
+    it('SMT.01 - should retrieve available transitions for current state', () => {
+      const sm = getStateMachine()
+      expect(sm.availableTransitions()).toEqual([
+        TestStates.State1,
+        TestStates.State2,
+      ])
+    })
+
+    it('SMT.02 - should retrieve available transitions for another state', () => {
+      const sm = getStateMachine()
+      expect(sm.availableTransitions(TestStates.State2)).toEqual([
+        TestStates.State1,
+      ])
+    })
+  })
+
+  describe('SMC - can transition', () => {
+    it('SMC.01 - can transition from current state', () => {
+      const sm = getStateMachine()
+      expect(sm.canTransitionTo(TestStates.State2)).toBeTruthy()
+    })
+
+    it('SMC.02 - cant transition from current state', () => {
+      const sm = getStateMachine(TestStates.State2)
+      expect(sm.canTransitionTo(TestStates.State2)).toBeFalsy()
+    })
+
+    it('SMC.02 - can transition from another state', () => {
+      const sm = getStateMachine()
+      expect(
+        sm.canTransition(TestStates.State1, TestStates.State2),
+      ).toBeTruthy()
+    })
+
+    it('SMC.02 - cant transition from another state', () => {
+      const sm = getStateMachine()
+      expect(
+        sm.canTransition(TestStates.State2, TestStates.State2),
+      ).toBeTruthy()
+    })
+  })
+
+  describe('SMX - transition', () => {
+    it('SMX.01 - should transition when possible', () => {
+      const sm = getStateMachine()
+
+      expect(sm.currentState).toEqual(TestStates.State1)
+      expect(sm.transitionTo(TestStates.State2)).toBeTruthy()
+      expect(sm.currentState).toEqual(TestStates.State2)
+    })
+
+    it('SMX.02 - should not transition when not possible', () => {
+      const sm = getStateMachine(TestStates.State3)
+
+      expect(sm.currentState).toEqual(TestStates.State3)
+      expect(sm.transitionTo(TestStates.State2)).toBeFalsy()
+      expect(sm.currentState).toEqual(TestStates.State3)
+    })
+  })
+})
---
+++ packages/state-machine/src/stateMachine.ts
@@ -0,0 +1,62 @@
+import debug from 'debug'
+
+const log = debug('PX:STM:StateMachine         ')
+
+export class StateMachine<TStateEnum extends string> {
+  constructor(
+    private stateTransitions: { [key in TStateEnum]: TStateEnum[] },
+    private current: TStateEnum,
+  ) {
+    log('Created new state machine with transitions %o', stateTransitions)
+  }
+
+  /**
+   * Gets the available transitions from the given state (or the given state)
+   * if `from` is left undefined
+   *
+   * @param from The state to transition from (the current state if left undefined)
+   */
+  public availableTransitions(from?: TStateEnum): TStateEnum[] {
+    return this.stateTransitions[from || this.current]
+  }
+
+  /**
+   * Returns true if the given `from` and `to` transitions are a valid transition
+   *
+   * @param from The state being transitioned from
+   * @param to The state being transitioned to
+   */
+  public canTransition(from: TStateEnum, to: TStateEnum): boolean {
+    return this.stateTransitions[from].includes(to)
+  }
+
+  /**
+   * Returns true if it is possible to transition from the current state to
+   * the given `to` state.
+   *
+   * @param to The state being transitioned to
+   */
+  public canTransitionTo(to: TStateEnum): boolean {
+    return this.canTransition(this.current, to)
+  }
+
+  public transitionTo(to: TStateEnum): boolean {
+    if (!this.canTransitionTo(to)) {
+      log(
+        `Unable to transition from ${this.current} to ${to}, aborting transition`,
+      )
+      return false
+    }
+
+    log(`Transitioning from ${this.current} to ${to}`)
+    this.current = to
+    return true
+  }
+
+  /**
+   * Gets the current state
+   */
+  public get currentState(): TStateEnum {
+    return this.current
+  }
+}
