2c15f18837f955981e5d548a40d855dc01ce0890
Will Hart <hart.wl@gmail.com>
2020-08-16 12:32:40 +1000

    Restructure app, add useWindowSize

--- packages/client-cra/src/App.tsx
+++ packages/client-cra/src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import { useWindowSize } from './hooks/useWindowSize'
+import { FullContainer } from './components'
 
 function App() {
+  const windowSize = useWindowSize()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FullContainer>{JSON.stringify(windowSize)}</FullContainer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ packages/client-cra/src/components/index.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const FullContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`
